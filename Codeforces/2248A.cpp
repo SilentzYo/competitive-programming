69699075dbe9ca1f6f03420b172b91eb1e249855
Hayden Leung <63174983+SilentzYo@users.noreply.github.com>
2026-09-02 20:42:41 -0700

    2248A works

--- Codeforces/2248A.cpp
+++ Codeforces/2248A.cpp
@@ -1,21 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string bitdelete() {
-    string s; cin >> s;
-    int zero = s.length()+1, one = -1;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '0') zero = min(zero, i);
-        else one = max(one, i);
-    }
-    s.erase(one, 1);
-    s.erase(zero, 1);
-    return s;
-}
-
-
 int main() {
 	int t; cin >> t;
-    while (t--) cout << bitdelete() << "\n";
+    while (t--) {
+        string s; cin >> s;
+        s.erase(s.find('1'), 1);
+        s.erase(s.find('0'), 1);
+        cout << s << "\n";
+    }
     return 0;
 }
